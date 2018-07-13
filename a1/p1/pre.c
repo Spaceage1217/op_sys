4ddcfab4b1ad2293a9ff8af2b9d3d01bd7826a02
Michael Scales <mhs42@txstate.edu>
2018-07-13 11:40:52 -0500

    add work on buffer

--- a1/p1/pre.c
+++ a1/p1/pre.c
@@ -4,17 +4,19 @@
 
 int main(int argc, char* argv[])
 {
-   int entries = 500;
+   int entries = 5;
    char *line = NULL;
    size_t size;
    char** buffer = (char**)malloc(entries * sizeof(char*));
-   while(getline(&line, &size, stdin) != EOF) {
-      //buffer[strcspn(buffer, "\n")] = 0;
-      //printf("%s\n", buffer);
+   for (int i = 0; i < entries; i++) buffer[i] = NULL;
+   while (getline(&line, &size, stdin) != EOF) {
+      line[strcspn(line, "\n")] = 0;
+      printf("%s\n", line);
       /*if (size > 0) printf("%s\n", line);
       else printf("No line.\n");
       printf("%zu\n", size);*/
    }
+   for (int i = 0; i < entries; i++) if (buffer[i]) free(buffer[i]);
    free(buffer);
    exit(EXIT_SUCCESS);
 }
