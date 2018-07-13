aeb8c4f0b1e29648644e74bcea7b07fc151dbb0b
Michael Scales <mhs42@txstate.edu>
2018-07-13 11:25:01 -0500

    adding buffer for strings

--- a1/p1/pre.c
+++ a1/p1/pre.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
+   int entries = 500;
    char *line = NULL;
    size_t size;
+   char** buffer = (char**)malloc(entries * sizeof(char*));
    while(getline(&line, &size, stdin) != EOF) {
-      if (size > 0) printf("%s\n", line);
+      //buffer[strcspn(buffer, "\n")] = 0;
+      //printf("%s\n", buffer);
+      /*if (size > 0) printf("%s\n", line);
       else printf("No line.\n");
-      printf("%zu\n", size);
+      printf("%zu\n", size);*/
    }
+   free(buffer);
    exit(EXIT_SUCCESS);
 }
