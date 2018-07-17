53295be7e5bd8793b3a23ab224dccaed5c3d6ca3
Mike Scales <mhs42@txstate.edu>
2018-07-16 20:55:53 -0500

    add a1-p1

--- a1/p1/pre.c
+++ a1/p1/pre.c
@@ -2,21 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[])
+static int ENTRIES = 12;
+
+void get_input(char **buffer);
+void print_output(char **buffer);
+
+int main(int argc, char *argv[])
 {
-   int entries = 5;
-   char *line = NULL;
-   size_t size;
-   char** buffer = (char**)malloc(entries * sizeof(char*));
-   for (int i = 0; i < entries; i++) buffer[i] = NULL;
-   while (getline(&line, &size, stdin) != EOF) {
+  char **buffer = (char **)malloc(ENTRIES * sizeof(char *));
+  for (int i = 0; i < ENTRIES; i++)
+    buffer[i] = NULL;
+  get_input(buffer);
+  print_output(buffer);
+  for (int i = 0; i < ENTRIES; i++)
+    if (buffer[i])
+      free(buffer[i]);
+  free(buffer);
+  exit(EXIT_SUCCESS);
+}
+
+void get_input(char **buffer)
+{
+  int i = 0;
+  size_t size;
+  char *line;
+  while (1)
+  {
+    int examCount;
+    printf("Enter a name (Ctrl-D to terminate the input):\t");
+    // Quit if we're done or before we run off the array
+    if (getline(&line, &size, stdin) == EOF || i >= ENTRIES)
+      break;
+
+    printf("Enter the number of courses:\t");
+    scanf("%d", &examCount);
+    while (fgetc(stdin) != '\n')
+      ; // Read until a newline is found
+
+    if (examCount > 4)
+    {
+      // Trim newline
       line[strcspn(line, "\n")] = 0;
-      printf("%s\n", line);
-      /*if (size > 0) printf("%s\n", line);
-      else printf("No line.\n");
-      printf("%zu\n", size);*/
-   }
-   for (int i = 0; i < entries; i++) if (buffer[i]) free(buffer[i]);
-   free(buffer);
-   exit(EXIT_SUCCESS);
+      buffer[i] = (char *)malloc(sizeof(char) * (size + 10));
+      strncpy(buffer[i++], line, size);
+    }
+  }
+  printf("\n");
+}
+
+void print_output(char **buffer)
+{
+  printf("Printing students with 5 or more courses:\n");
+  int i = 0;
+  while (buffer[i] != NULL)
+    printf("%s\n", buffer[i++]);
+  printf("\n");
 }
