2a10f5395dd590314d5f18830364a674fe726e5b
Mike Scales <mhs42@txstate.edu>
2018-07-24 22:03:14 -0500

    working minus the pipes

--- a2/main.c
+++ a2/main.c
@@ -1,6 +1,7 @@
 /*			Lab 2 Partial Example			*/
 
 #include <ctype.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -19,13 +20,10 @@ void trim(char *str);
 int main()
 {
   int i, status, procCount;
-  // int left_fd[2];
-  // int right_fd[2];
+  int left_fd[2], right_fd[2];
   bool redir_out, redir_in;
-  char *line;
-  char buf[256];
-  char infile[256];
-  char outfile[256];
+  char *line = NULL;
+  char buf[256], infile[256], outfile[256];
   char **args = (char **)malloc(MAX_ARGS * sizeof(char *));
   for (i = 0; i < MAX_ARGS; i++)
     args[i] = NULL;
@@ -52,55 +50,87 @@ int main()
     strcpy(buf, line);
     tokenize(buf, args, &procCount);
 
+    if (*args[0] == '\0') continue;
+
     findRedirect(args[0], infile, "<");
     if (infile[0] != '\0') redir_in = true;
 
     findRedirect(args[procCount - 1], outfile, ">");
     if (outfile[0] != '\0') redir_out = true;
 
-    // If pipes exist
-    if (procCount > 1) {
-      printf("has pipes\n");
-      // if (pipe(left_fd) < 0)
-      //   exit(EXIT_FAILURE);
-      // if (pipe(right_fd) < 0)
-      //   exit(EXIT_FAILURE);
-    }
-    else printf("single process\n");
-
     for (i = 0; i < procCount; i++)
     {
+      // If pipes exist
+      if (procCount > 1) {
+        if (pipe(left_fd) < 0 || pipe(right_fd) < 0)
+          exit(EXIT_FAILURE);
+      }
+
       // Proc area
       if((pid = fork()) > 0) {
         if (i > 0) {
-          // middle children stuff
-          // close(left_fd[0]);
-          // close(left_fd[1]);
-          printf("config pipes for mid proc\n");
+          close(left_fd[0]);
+          close(left_fd[1]);
         }
-        // left_fd[0] = right_fd[0];
-        // left_fd[1] = right_fd[1];
-        // parent stuff
-        printf("configured pipes for process\n");
+        left_fd[0] = right_fd[0];
+        left_fd[1] = right_fd[1];
 
-        // Wait for child to finish before starting again
+        // Wait for child to finish before proceeding
         if ((waitpid(pid, &status, 0) == pid) && (status != 0))
-          perror("Excuted process.\n");
+          fprintf(stderr, "Child process failed to execute.\n");
       } else if (pid == 0) {
-        
-        // Manage files for redirection
+        sleep(1);
+        // Input redirection
         if (i == 0 && redir_in) {
-          printf("Input: %s\n", infile);
+          int fdIn = open(infile, O_RDONLY);
+          if (fdIn < 0) perror(outfile);
+          dup2(fdIn, 0);
+          close(fdIn);
         }
-        if (i == procCount - 1 && redir_out) {
-          printf("Output: %s\n", outfile);
+
+        // Pipes
+        if (i < procCount - 1) {
+          dup2(right_fd[0], 0);
+          dup2(right_fd[1], 1);
+        }
+
+        // Output redirection
+        if (i == procCount - 1) {
+          if (redir_out) {
+            int fdOut = open(outfile, O_WRONLY | O_TRUNC | O_CREAT, 0755);
+            if (fdOut < 0) perror(outfile);
+            dup2(fdOut, 1);
+            close(fdOut);
+          } else dup2(right_fd[1], 1);
         }
     
-        char *const argv[2] = { args[i], NULL };
+        // Parse arguments
+        int argLength = strlen(args[i]);
+        char *argCopy = (char*)malloc(argLength + 1 * sizeof(char));
+        strncpy(argCopy, args[i], argLength);
+
+        i = 0;
+        char *argv[15] = {};
+        char *arg = strtok(argCopy, " ");
+        do {
+          int subArgLen = strlen(arg);
+          argv[i] = (char*)malloc((subArgLen + 1) * sizeof(char));
+          strncpy(argv[i++], arg, subArgLen);
+        } while ((arg = strtok(NULL, " ")) && i < 15);
+        argv[i] = NULL;
+        free(argCopy);
         execvp(argv[0], argv);
         // Shouldn't get here
-        fprintf(stderr, "Process { %s } didn't exec.\n", args[i]);
-      } else perror("Something broke");
+        perror(args[i]);
+        exit(EXIT_FAILURE);
+      } else perror("Pipe");
+
+      if (procCount > 1) {
+        close(left_fd[0]);
+        close(left_fd[1]);
+        close(right_fd[0]);
+        close(right_fd[1]);
+      }
     }
   }
   for (i = 0; i < MAX_ARGS; i++)
@@ -120,7 +150,7 @@ void tokenize(char *line, char **args, int *procCount)
   {
     trim(cmd);
     size = strlen(cmd);
-    args[i] = (char *)malloc(size * sizeof(char));
+    args[i] = (char *)malloc(size + 1 * sizeof(char));
     strncpy(args[i++], cmd, size);
     cmd = strtok(NULL, "|");
   }
@@ -132,19 +162,16 @@ void tokenize(char *line, char **args, int *procCount)
 
 void findRedirect(char *arg, char *reFile, char *delim)
 {
-  //printf("Testing: %s\n", arg);
   char *match = strtok(arg, delim);
   if (match != NULL)
   {
     trim(match);
-    // printf("arg: %s\n", match);
     strcpy(arg, match);
   } else return;
   
   match = strtok(NULL, " ");
   if (match != NULL) {
     trim(match);
-    // printf("infile: %s\n", match);
     strcpy(reFile, match);
   }
 }
