53c441d58cc9d580517258cadfff812fb7d02f1c
Mike Scales <mhs42@txstate.edu>
2018-08-02 22:10:32 -0500

    cleaning up code

--- a3/threadPartA.c
+++ a3/threadPartA.c
@@ -11,12 +11,13 @@
 
 struct Manufacturer
 {
-  int itemsGiven;
+  int itemsTaken;
   int items;
 };
 
 struct Salesman
 {
+  pthread_t tid;
   char name;
   double takes;
 };
@@ -27,35 +28,38 @@ struct Manufacturer mfr;
 
 int main()
 {
-  pthread_t tid[3];
-
-  mfr.itemsGiven = 0;
+  mfr.itemsTaken = 0;
   mfr.items = CAPACITY;
-  struct Salesman s1 = {'A', 0.33};
-  struct Salesman s2 = {'B', 0.25};
-  struct Salesman s3 = {'C', 0.2};
+
+  struct Salesman s1 = { 0, 'A', 0.33 };
+  struct Salesman s2 = { 0, 'B', 0.25 };
+  struct Salesman s3 = { 0, 'C', 0.2 };
 
   pthread_setconcurrency(3);
 
-  pthread_create(&tid[0], NULL, (void *(*)(void *))getShirts, &s1);
-  pthread_create(&tid[1], NULL, (void *(*)(void *))getShirts, &s2);
-  pthread_create(&tid[2], NULL, (void *(*)(void *))getShirts, &s3);
+  pthread_create(&s1.tid, NULL, (void *(*)(void *))getShirts, &s1);
+  pthread_create(&s2.tid, NULL, (void *(*)(void *))getShirts, &s2);
+  pthread_create(&s3.tid, NULL, (void *(*)(void *))getShirts, &s3);
 
-  for (int i = 0; i < 3; i++)
-       pthread_join(tid[i], NULL);
-  printf("The total number of T-shirts given out it: %d\n", mfr.itemsGiven);
+  pthread_join(s1.tid, NULL);
+  pthread_join(s2.tid, NULL);
+  pthread_join(s3.tid, NULL);
+
+  printf("The total number of T-shirts given out is: %d\n", mfr.itemsTaken);
   exit(EXIT_SUCCESS);
 }
 
 void *getShirts(struct Salesman *s)
 {
+  int shirtsTaken = 0;
   while (mfr.items > 0)
   {
-    sleep(SLEEP_INTERVAL);
-    int remItems = (int) ceil(mfr.items * s->takes);
+    int remItems = (int)ceil(mfr.items * s->takes);
+    shirtsTaken += remItems;
     mfr.items -= remItems;
     printf("%c takes away %d T-shirts.\n", s->name, remItems);
-    mfr.itemsGiven += remItems;
+    sleep(SLEEP_INTERVAL);
   }
+  mfr.itemsTaken += shirtsTaken;
   return 0;
 }
